7024a477ea6b15dfd64460a447182827fe36bf53
Tyuiu.PyankovaVV@outlook.com <vika.pyankova04@mail.ru>
2023-10-05 21:01:26 +0500

    Task1

--- Tyuiu.PyankovaVV.Sprint2.Task1.V30/Program.cs
+++ Tyuiu.PyankovaVV.Sprint2.Task1.V30/Program.cs
@@ -12,6 +12,53 @@ namespace Tyuiu.PyankovaVV.Sprint2.Task1.V30
     {
         static void Main(string[] args)
         {
+            DataService ds = new DataService();
+
+            int a = 657;
+            int b = 874;
+            int c = 14;
+            int d = 654;
+
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d);
+
+            Console.Title = "Спринт #1 | Выполнила: Пьянкова В. В. | АСОиУб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Cпринт №2                                                               *");
+            Console.WriteLine("* Задание №1                                                              *");
+            Console.WriteLine("* Вариант №30                                                             *");
+            Console.WriteLine("* Выполнила: Пьянкова Виктория Вячеславовна / АСОиУб-23-2                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений                                *");
+            Console.WriteLine("* (==, !=, <, >, <=, >=, последовательность можно чередовать,             *");
+            Console.WriteLine("* но использовать один раз в выражении) и логических операций             *");
+            Console.WriteLine("* (|, &, ||, &&, !, ^, последовательность операций не должна нарушаться), *");
+            Console.WriteLine("* а также арифметических выражений,                                       *");
+            Console.WriteLine("* которая вернет логическую последовательность(массив):                   *");
+            Console.WriteLine("* (True, False, True, True, True, False),                                 *");
+            Console.WriteLine("* при a = 657, b = 874, c = 14, d = 654                                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("A = " + a);
+            Console.WriteLine("B = " + b);
+            Console.WriteLine("C = " + c);
+            Console.WriteLine("D = " + d);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+
+
+            Console.ReadKey();
         }
     }
 }

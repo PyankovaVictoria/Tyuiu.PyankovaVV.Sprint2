59189bac66511b38cb8113f540a8e07ebe4ef88a
Tyuiu.PyankovaVV@outlook.com <vika.pyankova04@mail.ru>
2023-10-06 20:55:00 +0500

    Task2

---
+++ Tyuiu.PyankovaVV.Sprint2.Task2.V3.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PyankovaVV.Sprint2.Task2.V3.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PyankovaVV.Sprint2.Task2.V3.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("49b598ab-a568-44e3-9902-a916e1eeecc6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.PyankovaVV.Sprint2.Task2.V3/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.PyankovaVV.Sprint2.Task2.V3.Lib;
+
+namespace Tyuiu.PyankovaVV.Sprint2.Task2.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Пьянкова В. В. | АСОиУб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Алгоритмы разветвляющей структуры. Оператор if                    *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнила: Пьянкова Виктория Вячеславовна | АСОиУб-23-2                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("*Написать программу, которая запрашивает целые значения с клавиатуры и    *");
+            Console.WriteLine("*вычисляет находится ли точка с координатами X,Y в заштрихованной области *");
+            Console.WriteLine("***************************************************************************");
+
+            int x;
+            int y;
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(" Введите значение переменной х: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" Введите значение переменной y: ");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            if (res == true)
+                Console.WriteLine("Точка с данными координатами является заштрихованной");
+            else
+                Console.WriteLine("Точка с данными координатами не является заштрихованной");
+
+            Console.ReadKey();
+        }
+    }
+}

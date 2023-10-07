20cd085b3db942f6e4f19768b71567e373a433a1
Tyuiu.PyankovaVV@outlook.com <vika.pyankova04@mail.ru>
2023-10-07 20:12:19 +0500

    Task3

---
+++ Tyuiu.PyankovaVV.Sprint2.Task3.V1.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.PyankovaVV.Sprint2.Task3.V1.Lib;
+
+namespace Tyuiu.PyankovaVV.Sprint2.Task3.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void VoidExpression()
+        {
+            DataService ds = new DataService();
+
+            double x = 5;
+
+            var res = ds.Calculate(x);
+            Assert.AreEqual(88.697, res);
+        }
+    }
+}
---
+++ Tyuiu.PyankovaVV.Sprint2.Task3.V1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.PyankovaVV.Sprint2.Task3.V1.Lib;
+
+namespace Tyuiu.PyankovaVV.Sprint2.Task3.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Пьянкова В. В. | АСОиУб-23-2";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Создание итоговых проектов по спринту                             *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнила: Пьянкова Виктория Вячеславовна | АСОиУб-23-2                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Y      *");
+            Console.WriteLine("* с использованием вложенных оператор if-else,                            *");
+            Console.WriteLine("* где пользователь вводит значение переменной X с клавиатуры.             *");
+            Console.WriteLine("* Округлить полученное значение до трех знаков после запятой.             *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double x;
+
+            Console.WriteLine("Введите значение переменной Х: ");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Результат: " + ds.Calculate(x));
+            Console.ReadKey();
+        }
+    }
+}

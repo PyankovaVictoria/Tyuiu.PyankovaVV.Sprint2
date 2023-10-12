ca36b879231cb0f197f1a378f609e77bb09bd038
Tyuiu.PyankovaVV@outlook.com <vika.pyankova04@mail.ru>
2023-10-12 19:56:25 +0500

    Task5

---
+++ Tyuiu.PyankovaVV.Sprint2.Task5.V3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.PyankovaVV.Sprint2.Task5.V3.Lib;
+
+namespace Tyuiu.PyankovaVV.Sprint2.Task5.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт#2 |Выполнила: Пьянкова В. В.|АСОиУб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт#2                                                                *");
+            Console.WriteLine("* Тема: Оператор switch                                                   *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнила: Пьянкова Виктория Вячеславовна | АСОиУб-23-2                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая использует оператор switch вычисляет        *");
+            Console.WriteLine("* требуемое значение и возвращает результат.                              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int value;
+            Console.WriteLine("Введите номер дня недели: ");
+            value = Convert.ToInt32(Console.ReadLine());
+            string day;
+            if ((value < 1) || (value > 7))
+            {
+                day = "Неверно введенное значение!";
+            }
+            else
+            {
+                day = $"День недели с введенным номером {value} - " + ds.FindDayName(value);
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(day);
+            Console.ReadKey();
+
+        }
+    }
+}
